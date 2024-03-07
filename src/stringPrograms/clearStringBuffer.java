36b9c6922d9fd1e521303323bc7fd3d88c32df1c
Prajith-97 <58196671+Prajith-97@users.noreply.github.com>
2024-03-07 08:19:54 +0530

    Update clearStringBuffer.java

--- src/stringPrograms/clearStringBuffer.java
+++ src/stringPrograms/clearStringBuffer.java
@@ -5,7 +5,7 @@ public class clearStringBuffer {
         StringBuffer sb = new StringBuffer();
         sb.append("abcasdadff");
         System.out.println(sb.toString());
-        sb.delete(1,5);
+        sb.delete(1,5);//Deletes characters from index 1 to index 4 (inclusive) from the StringBuffer.
         System.out.println(sb.toString());
     }
     public static void main(String[] args){
